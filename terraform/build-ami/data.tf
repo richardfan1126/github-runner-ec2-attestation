cf21786558e35b488d51239e68d83e8547c6a331
Richard Fan <richardfan1126@gmail.com>
2026-04-20 14:11:10 +0800

    chore: pin AL2023 AMI to al2023-ami-2023.11.20260413.0-kernel-6.1-x86_64

--- terraform/build-ami/data.tf
+++ terraform/build-ami/data.tf
@@ -14,7 +14,7 @@ data "aws_ami" "amazon_linux_2023" {
 
   filter {
     name   = "name"
-    values = ["al2023-ami-2023.7.20250512.0-kernel-6.1-x86_64"]
+    values = ["al2023-ami-2023.11.20260413.0-kernel-6.1-x86_64"]
   }
 
   filter {
